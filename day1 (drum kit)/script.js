42442710eab7381cbab92b896b6fe0f5efb5e21e
PavelMishin <trobing1@gmail.com>
2018-11-25 12:37:00 +0600

    improved day 1 drum kit - centered buttons, added hi-hat, added click handler, refactored

--- day1 (drum kit)/script.js
+++ day1 (drum kit)/script.js
@@ -1,23 +1,31 @@
-window.addEventListener('keydown', playTheSound);
+window.addEventListener('keydown', playTheSound); // On each key press
 
 const instruments = document.querySelectorAll('.instrument');
-instruments.forEach(instrument => instrument.addEventListener('transitionend', backToDefault));
+instruments.forEach(instrument => instrument.addEventListener('transitionend', backToDefault)); // On the end of transition remove playing style
+instruments.forEach(instrument => instrument.addEventListener('click', playTheSound)); // Play sound on click
 
 
 function backToDefault(e) {
-	if (e.propertyName !== 'transform') return;
+	if (e.propertyName !== 'transform') return; // if transition not transform property skip it
 	this.classList.remove('playing');
 }
 
 function playTheSound(e) {
-	const key = e.keyCode; // Получаем код нажимаемой клавиши
+	let key;
+    // Getting pressed key code
+	if (e.type === 'click') {
+		key = e.target.parentNode.dataset.key;
+	} else {
+		key = e.keyCode;
+	}
+
 	const selector = `[data-key="${key}"]`;
-	const audio = document.querySelector(`audio${selector}`);
-	const visual = document.querySelector(`.instrument${selector}`);
+	const audio = document.querySelector(`audio${selector}`); // Get audio element
+	const visual = document.querySelector(`.instrument${selector}`); // Get button element
 
 	if (!audio) return;
 	
-	audio.play(); // Проигрываем звук
-	audio.currentTime = 0; // Сбрасываем playback звука, чтобы не ждать пока файл проиграется до конца
+	audio.play();
+	audio.currentTime = 0; // Reset track playback time to start
 	visual.classList.add('playing');
 }
